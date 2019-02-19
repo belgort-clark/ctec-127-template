421a9a3801257b148d75cc37ada59382aacfcf7e
belgort-clark <belgort@clark.edu>
2019-02-19 06:11:10 -0800

    Minor updates

--- page.php
+++ page.php
@@ -10,9 +10,18 @@
     <title>Page Title</title>
 </head>
 <body>
-
-
-
+<div class="container">
+    <section>
+        <h1>Evergreen</h1>
+        <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Voluptates at reiciendis expedita adipisci sint? Esse quas repellendus obcaecati dicta. Iusto voluptates dicta dolor omnis necessitatibus labore amet voluptatem distinctio repudiandae.</p>
+        <h2>Camas</h2>
+        <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Eius enim, minus cumque asperiores libero consectetur. Praesentium sint itaque, eaque assumenda expedita totam dignissimos molestias sequi natus placeat, aliquid sapiente atque.</p>
+        <h3>Vanmcouver</h3>
+        <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Iusto sit ad, aspernatur autem temporibus et ipsam, consequatur excepturi delectus, quo ratione! Quam explicabo repudiandae commodi delectus saepe, adipisci laborum quaerat!</p>
+        <h4>Comsewogue</h4>
+        <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quibusdam dolores eos quasi nostrum ducimus nam pariatur molestias consequuntur mollitia recusandae illo accusantium laboriosam debitis minus, quia, cum omnis. Quod, et!</p>
+    </section>
+</div>
 
 <!-- jQuery -->
 <script src="js/jquery-3.3.1.min.js"></script>
